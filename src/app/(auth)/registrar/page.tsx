ef688152040c85e219a33e433a6304f2113b40cf
Pedro Valério <pedroddjkK@gmail.com>
2023-09-17 02:15:57 -0300

    Lucia Implemented

---
+++ src/app/(auth)/registrar/page.tsx
@@ -0,0 +1,27 @@
+import Form from "@/components/form";
+import Link from "next/link";
+import { auth } from "@/auth/lucia";
+import * as context from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const authRequest = auth.handleRequest("GET", context);
+  const session = await authRequest.validate();
+  if (session) redirect("/");
+
+  return (
+    <>
+      <h1>Sign up</h1>
+      <Form action="/api/signup">
+        <label htmlFor="username">Username</label>
+        <input name="username" id="username" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <br />
+        <input type="submit" />
+      </Form>
+      <Link href="/login">Sign in</Link>
+    </>
+  );
+}
