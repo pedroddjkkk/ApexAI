8111514e0706412ef794c0b005f8ed9090851c38
Pedro Valério <pedroddjkK@gmail.com>
2023-10-31 16:43:09 -0300

    Arrumando .env e Webhook

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const signature = req.headers.get('x-signature')
+}
