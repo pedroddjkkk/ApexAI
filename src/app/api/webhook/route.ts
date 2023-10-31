c0c7751df3b09bb08ec6530346c2f51ce638a366
Pedro Valério <pedroddjkK@gmail.com>
2023-10-31 17:05:10 -0300

    Webhook Implementado

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,7 +1,14 @@
-import { NextRequest } from "next/server";
+import { verifyWebhook } from "@/lib/webhook/verify";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
 
-  const signature = req.headers.get('x-signature')
+  const valid = await verifyWebhook(req);
+
+  if (!valid) {
+    return NextResponse.json("Invalid signature", { status: 401 });
+  }
+
+  return NextResponse.json(body);
 }
--- src/lib/webhook/verify.ts
+++ src/lib/webhook/verify.ts
@@ -1,5 +1,27 @@
 import { NextRequest } from "next/server";
+import bcrypt from "bcrypt";
 
 export function verifyWebhook(signature: string | NextRequest) {
-  
+  const signatureHeader =
+    typeof signature === "string"
+      ? signature
+      : signature.headers.get("x-signature");
+
+  if (!signatureHeader) {
+    throw new Error("No signature header provided");
+  }
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(
+      process.env.SECRET_WEBHOOK_KEY ?? "",
+      signatureHeader,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 }
