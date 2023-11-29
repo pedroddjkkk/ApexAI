259407978b809b8759b08f5d104ef65045ebfccb
Renan0eng <renan_terra_rica@hotmail.com.br>
2023-11-29 18:40:58 -0300

    feat: Add FAQ form component and refactor AI config view
    This commit introduces a new FAQ form component and refactors the AI
    configuration view. The FAQ form allows users to add questions and
    responses, and supports validation for required fields. The AI
    configuration view now uses the updated form component and includes
    options for adding and importing FAQs. The pagination functionality for
    displaying FAQs has been removed in favor of a data table component.
    These changes enhance the user experience by providing a more intuitive
    and streamlined interface for managing FAQs in the AI configuration.
    Resolves #123.

--- src/components/form/popoverFormFaq.tsx
+++ src/components/form/popoverFormFaq.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -6,56 +7,102 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import z from "zod"
+import { useState } from "react"
+import { cva, type VariantProps } from "class-variance-authority"
 
 type Props = {
   children: React.ReactNode
-  onChange: (e: any) => void
-}
-
-type Inputs = {
-  question: string
+  onChange: (e: {
+    quest: string
+    response: string
+    id: string
+  }) => void
+  open: boolean
+  setOpen: (e: boolean) => void
   response: string
+  setResponse: (e: string) => void
+  quest: string
+  setQuest: (e: string) => void
+  questId?: string
+  setQuestId: (e: string) => void
 }
 
-export function PopoverFormFaq({ children, onChange }: Props) {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    setValue,
-    reset,
-    formState: { errors },
-  } = useForm<Inputs>({
-    resolver: zodResolver(z.object({
-      question: z.string().nonempty("Campo obrigatório"),
-      response: z.string().nonempty("Campo obrigatório"),
-    })),
-    defaultValues: {
-      question: "",
-      response: "",
-    },
-  });
+export function PopoverFormFaq({
+  children,
+  onChange,
+  open,
+  setOpen,
+  response,
+  setResponse,
+  quest,
+  setQuest,
+  questId,
+  setQuestId
+}: Props) {
+
+  const btn = cva(
+    "w-full",
+    {
+      variants: {
+        variant: {
+          default: "bg-success-500/90 hover:bg-success-500",
+          edit: "bg-blue-500 hover:bg-blue-700",
+        },
+      },
+      defaultVariants: {
+        variant: "default",
+      },
+    }
+  )
+
+
+  const [errors, setErrors] = useState("")
+
+  const [id, setId] = useState(1)
+
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={(e) => {
+      if (!e) {
+        setErrors('')
+        setQuest('')
+        setResponse('')
+        setQuestId('')
+      }
+      setOpen(e)
+    }}>
       <PopoverTrigger asChild>
         {children}
       </PopoverTrigger>
       <PopoverContent className="w-80">
-        <form className="p-4" >
+        <div className="p-4" >
           <Label className="mb-2">Pergunta</Label>
-          <Input className="mb-4" name="question" />
-          <Label className="mb-2">Pesposta</Label>
-          <Input className="mb-4" name='response' />
-          <Button className="w-full bg-success-500/90 hover:bg-success-500"
-            onClick={(e) => {
-              e.preventDefault()
-              handleSubmit(onChange)
+          <Input className="mb-4" value={quest} onChange={(e) => setQuest(e.target.value)} />
+          <Label className="mb-2">Resposta</Label>
+          <Input className="mb-4" value={response} onChange={(e) => setResponse(e.target.value)} />
+          <span className="text-red-500">{errors}</span>
+          <Button className={btn({ variant: questId ? "edit" : "default" })}
+            onClick={() => {
+              if (!quest) {
+                setErrors("Pergunta é campo obrigatório")
+                return
+              }
+              if (!response) {
+                setErrors("Resposta é campo obrigatório")
+                return
+              }
+              onChange({ quest, response, id: questId ? questId : id.toString() })
+              setId(id + 1)
+              setQuest('')
+              setResponse('')
+              setErrors('')
+              setQuestId('')
+              // fecha o popover do chadcn ui
+              setOpen(false)
             }}
-          >Salvar</Button>
-        </form>
+          >{
+              questId ? "Editar" : "Adicionar"
+            }</Button>
+        </div>
       </PopoverContent>
     </Popover>
   )
