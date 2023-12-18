d23b23e92c06b54b7f62497093687fbe37cc5baf
Renan0eng <renan_terra_rica@hotmail.com.br>
2023-12-18 06:28:51 -0300

    Optimized imports and improved null safety in GET
    Streamlined `route.ts` by removing unused imports and refactoring to
    only import the `getAllAiConfigs` function. Enhanced null handling by
    opting for optional chaining on user object access, ensuring more robust
    error responses when the user is not present. This not only cleans up
    the code by eliminating dead references but also improves the
    maintainability and readability of the route handler.
    
    Ref: #12345 (if applicable)

--- src/app/api/all-ai-config/route.ts
+++ src/app/api/all-ai-config/route.ts
@@ -1,47 +1,13 @@
 import { getServerSideSession } from "@/lib/session";
-import {
-  PropsCreateAiConfig,
-  createAiConfig,
-  deleteAiConfig,
-  getAiConfigs,
-  getAllAiConfigs,
-  saveFiles,
-  updateAiConfig,
-} from "@/model/ai-config";
+import { getAllAiConfigs } from "@/model/ai-config";
 import { NextRequest, NextResponse } from "next/server";
-
-type PropsForm = {
-  id?: string;
-  user_id: string;
-  name: string;
-  sistema: string;
-  max_tokens: number;
-  model: string;
-  temperature: number;
-  stop: string;
-  top_p: number;
-  frequency_penalty: number;
-  presence_penalty: number;
-  faq:
-    | {
-        id: string;
-        quest: string;
-        response: string | File;
-      }[]
-    | string;
-  files: {
-    name: string;
-    url: string;
-  }[];
-  action?: string;
-};
 
 export const GET = async (request: NextRequest) => {
   const user = await getServerSideSession();
 
-  if (!user.user) return NextResponse.json({ ret: "not found" });
+  if (!user?.user) return NextResponse.json({ ret: "not found" });
 
-  const ret = await getAllAiConfigs(user.user.userId);
+  const ret = await getAllAiConfigs(user?.user.userId);
 
   return NextResponse.json(ret);
 };
