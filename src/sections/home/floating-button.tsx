e75844561c4747cceb2c10cd1c455bd7366d012e
Renan0eng <renan_terra_rica@hotmail.com.br>
2023-12-18 06:37:09 -0300

    Adjust floating button positioning for mobile
    The floating button now has improved visibility on mobile devices by
    adjusting its placement closer to the bottom and side edges of the
    screen, ensuring better accessibility and thumb reach. The changes
    maintain the existing positioning for larger screens.
    
    Ref: #AdjustFloatingBtn-Mobile

--- src/sections/home/floating-button.tsx
+++ src/sections/home/floating-button.tsx
@@ -4,7 +4,7 @@ import { PiChatCircleText } from "react-icons/pi";
 export default function FloatingButton() {
 
   return (
-    <Button className="bg-primary-500 hover:bg-white hover:text-primary-500 px-3 py-3 rounded-full flex fixed bottom-16 right-16 text-white shadow h-15 w-15">
+    <Button className="bg-primary-500 hover:bg-white hover:text-primary-500 px-3 py-3 rounded-full flex fixed md:bottom-16 bottom-6 md:right-16 right-6 text-white shadow h-15 w-15">
       <PiChatCircleText size={35} />
     </Button>
   )
